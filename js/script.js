eb5bdc3ed65a370081fd2b0bc6ee42db37c6452e
emmepi92 <mariapiacoccioli@live.it>
2021-05-28 14:10:56 +0200

    better js

--- js/script.js
+++ js/script.js
@@ -3,19 +3,20 @@ var yearsInput = prompt("Quanti anni hai?")
 
 var km = parseInt(kmInput);
 var years = parseInt(yearsInput);
-document.getElementById('output').innerHTML = km + yearsInput;
+var outputElement = document.getElementById('output');
 
 var ticketPrice = km * 0.21;
+outputElement.innerHTML = 'Il prezzo del suo biglietto è € ' + ticketPrice ;
 
-document.getElementById('output').innerHTML = ticketPrice;
+
 
 if (years < 18) {
     ticketPrice = ticketPrice / 100 * 80;
-    document.getElementById('output').innerHTML ='Complimenti, ha diritto ad uno sconto del 20% sul prezzo del suo biglietto, il prezzo scontanto è € ' + ticketPrice;
+    outputElement.innerHTML +='<br/>Complimenti, ha diritto ad uno sconto del 20% sul prezzo del suo biglietto, il prezzo scontanto è € ' + ticketPrice;
 } else if (years >= 65) {
     ticketPrice = ticketPrice / 100 * 60;
-    document.getElementById('output').innerHTML ='Complimenti, ha diritto ad uno sconto del 40% sul prezzo del suo biglietto, il prezzo scontanto è € ' + ticketPrice;
+    outputElement.innerHTML +='<br/>Complimenti, ha diritto ad uno sconto del 40% sul prezzo del suo biglietto, il prezzo scontanto è € ' + ticketPrice;
 } else {
-    document.getElementById('output').innerHTML ='Il prezzo del suo biglietto è pari a € ' + ticketPrice;
+    outputElement.innerHTML ='Il prezzo del suo biglietto è pari a € ' + ticketPrice;
 }
 

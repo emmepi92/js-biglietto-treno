86bcb56ae97f562bfe2fe2c68ebe9d1d6f289b20
emmepi92 <mariapiacoccioli@live.it>
2021-05-28 18:00:16 +0200

    bonus

---
+++ bonus/js_bonus/script_bonus.js
@@ -0,0 +1,13 @@
+var km = parseInt(prompt("Quanti chilomentri è distante la sua meta?"));
+var year= parseInt(prompt("In che anno sei nato?"));
+
+var outputElement = document.getElementById('output');
+var ticketPrice = km * 0.21;
+
+if ((2021 - year) < 18) {
+    ticketPrice = ticketPrice / 100 * 80
+} else if ((2021 - year) >= 65) {
+    ticketPrice = ticketPrice / 100 * 60;
+}
+
+outputElement.innerHTML = "Prezzo Biglietto: € " + ticketPrice;
